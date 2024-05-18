709a6419339ea13004c75c4069954a0c74f05b55
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-05-18 08:59:05 +0545

    div animation

--- Rishi_portfilo/src/assets/components/card_left.jsx
+++ Rishi_portfilo/src/assets/components/card_left.jsx
@@ -37,7 +37,24 @@ class Cardleft extends React.Component {
               </li>
             </ul>
 
-            <button>Downlode CV</button>
+            <button class="animated-button">
+              <svg
+                viewBox="0 0 24 24"
+                class="arr-2"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+              </svg>
+              <span class="text">CV</span>
+              <span class="circle">Download</span>
+              <svg
+                viewBox="0 0 24 24"
+                class="arr-1"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+              </svg>
+            </button>
           </div>
         </div>
       </>
